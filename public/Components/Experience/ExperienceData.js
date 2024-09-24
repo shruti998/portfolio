0d4867b53ae432b506f81d79d9236ec036ccef30
Shruti Srivastava <srivastava.shru@northeastern.edu>
2024-09-23 20:37:42 -0400

    Adding new experience

--- public/Components/Experience/ExperienceData.js
+++ public/Components/Experience/ExperienceData.js
@@ -6,6 +6,15 @@ const ExperienceData = [
     {
       
         title: "Software Engineer",
+        cName:"Energy Services Group",
+        date:"July 2023 - Dec 2023",
+        tech: "Java, Testing, AWS Secret Manager",
+       
+
+    },
+    {
+      
+        title: "Software Engineer",
         cName:"Tata Consultancy Services ",
         date:"Sep 2020 - Aug 2022",
         tech: "Java, Python and MongoDB",
